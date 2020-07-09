cec18edf6f1d057f638cc07d83ba23b467776559
vanminh.le <vanminh.le.754@gmail.com>
2020-07-09 20:26:38 +0700

    Add files via upload

---
+++ Loop Demo/Loop 1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Loop_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 20;
+            while(a< 15)
+            {
+                b = b * a;
+                a++;
+            }
+            Console.WriteLine(b);
+
+        }
+    }
+}
---
+++ LowerOrUpper/LowerOrUpper/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LowerOrUpper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //enter character
+            Console.WriteLine("Enter Character: ");
+            char ch = char.Parse(Console.ReadLine());
+
+            //checking character
+            if (ch >= 'A' && ch <= 'Z') 
+            {
+                Console.WriteLine(ch + " is uppercase character");
+            }
+            else            
+            {
+                Console.WriteLine(ch + " is lowercase character");
+            }
+
+            
+        }
+    }
+}
