00316d14b8e27f1b342748babc03aae015941d39
vanminh.le <vanminh.le.754@gmail.com>
2020-07-09 20:18:34 +0700

    Add files via upload

---
+++ even number/even number/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace even_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            do
+            {
+                n = int.Parse(Console.ReadLine());
+            } while (n % 2 == 1);
+
+
+            if (n < 0)
+            {
+                n = -n;
+            }
+            Console.WriteLine("the number is {0}", n);
+        }
+    }
+}
