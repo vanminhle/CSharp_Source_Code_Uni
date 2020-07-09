b0d30388347d11856edcf577fae14ceea00ef107
vanminh.le <vanminh.le.754@gmail.com>
2020-07-09 21:05:48 +0700

    Add files via upload

---
+++ Reverse Strings/Reverse Strings/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Reflection.Metadata;
+
+namespace Reverse_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "", reverse = "";
+            int Length = 0;
+
+            Console.WriteLine("Enter a Word"); 
+            str = Console.ReadLine();  
+
+            Length = str.Length - 1;
+            while (Length >= 0)
+            {
+                reverse = reverse + str[Length];
+                Length--;
+            }
+
+            Console.WriteLine("Reverse word is {0}", reverse);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Sign Of Integer Numbers/Sign Of Integer Numbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Sign_Of_Integer_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Number: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            if (n > 0)
+            {
+                Console.WriteLine("The number {0} is positive", n);
+            }
+            else if (n < 0)
+            {
+                Console.WriteLine("The number {0} is negative", n);
+            }
+            else
+                Console.WriteLine("The number 0 is zero");
+        }
+    }
+}
