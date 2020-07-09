0fef62d48250d29d316476a077bcc058ff31d7e7
vanminh.le <vanminh.le.754@gmail.com>
2020-07-09 20:48:49 +0700

    Add files via upload

---
+++ Theatre Promotions/Theatre Promotions/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+
+namespace Theatre_Promotions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Day: ");
+            var day = Console.ReadLine().ToLower();
+            Console.Write("Enter Age: ");
+            var age = int.Parse(Console.ReadLine());
+            var price = 0;
+
+            if (day == "weekday")
+            {
+                if ((age >= 0 && age <= 18) || (age > 64 && age <= 122)) 
+                {
+                    price = 12;
+                }
+                else if(age > 18 && age <= 64)
+                {
+                    price = 18;
+                }
+                else if(age > 64 && age <= 122)
+                {
+                    price = 12;
+                }
+            }
+
+            else if (day == "weekend")
+            {
+                if ((age >= 0 && age <= 18) || (age > 64 && age <= 122)) 
+                {
+                    price = 15;
+                }
+                else if (age > 18 && age <= 64)
+                {
+                    price = 20;
+                }
+                else if (age > 64 && age <= 122)
+                {
+                    price = 15;
+                }
+            }
+
+            else if (day == "holiday")
+            {
+                if ((age >= 0 && age <= 18) || (age > 64 && age <= 122)) 
+                {
+                    price = 5;
+                }
+                else if (age > 18 && age <= 64)
+                {
+                    price = 12;
+                }
+                else if (age > 64 && age <= 122)
+                {
+                    price = 10;
+                }
+            }
+           
+
+            if (price != 0)
+            {
+                Console.WriteLine(price + "$");
+            }
+            else
+            {
+                Console.WriteLine("Error!");
+            }
+        }
+    }
+}
