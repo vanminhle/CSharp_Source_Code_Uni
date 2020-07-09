5c91b22707e72a7c0a750ea81b05198c8839b4e4
vanminh.le <vanminh.le.754@gmail.com>
2020-07-09 20:07:04 +0700

    Add files via upload

---
+++ AmduongArr/AmduongArr/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(" Enter the number of array elements (array <=0!) : ");
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            Console.WriteLine("input {0} Element in the array", n);
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine("value {0}: ", i);
+                arr[i] = int.Parse(Console.ReadLine());
+                if (arr[i] > 0)
+                {
+                    Console.WriteLine(" positive numbers");
+                }
+                else if (arr[i] < 0)
+                {
+                    Console.WriteLine(" negative number ");
+                }
+                else if (arr[i] == 0)
+                {
+                    Console.WriteLine(" not value ");
+                }
+
+
+
+
+
+            }
+
+        }
+    }
+}
