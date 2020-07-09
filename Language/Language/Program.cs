27aa75d80773749b786ae98b3b75ca21112154d5
vanminh.le <vanminh.le.754@gmail.com>
2020-07-09 20:21:59 +0700

    Add files via upload

---
+++ Language/Language/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace language
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string language = Console.ReadLine();
+            switch (language)
+            {
+                case "USA":
+                    Console.WriteLine("English");
+                    break;
+                case "England":
+                    Console.WriteLine("English UK");
+                    break;
+                case "Spain":
+                    Console.WriteLine("Spanish");
+                    break;
+                case "Japan":
+                    Console.WriteLine("Japanese");
+                    break;
+                default:
+                    Console.WriteLine("Invalid Country");
+                    break;
+
+
+
+
+            }
+        }
+    }
+}
